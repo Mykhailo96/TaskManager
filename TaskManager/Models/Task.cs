e4fd56dc252f7a51aeb552af3097c081f6087e14
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-06-21 22:57:02 +0300

    Add class Task

---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+    }
+}
