611973e4dd9cbf51379fe810b98aa8092d4106a7
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-06-22 23:49:16 +0300

    Add script for _Layout

---
+++ TaskManager/Scripts/_layout.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function() {
+    $('.reference').on('click', function (e) {
+        e.preventDefault();
+        var page = $(this).attr("href");
+        $("#partialDiv").load(page);
+    });
+})
