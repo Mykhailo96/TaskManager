627cd440290c125d7bb0f4f3b9b3ba3c70360f82
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-06-23 22:27:40 +0300

    Delete _layout.js

--- TaskManager/Scripts/_layout.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function() {
-    $('.reference').on('click', function (e) {
-        e.preventDefault();
-        var page = $(this).attr("href");
-        $("#partialDiv").load(page);
-    });
-})
