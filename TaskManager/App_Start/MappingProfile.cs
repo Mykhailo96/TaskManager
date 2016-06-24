1e3409ddaab5cd5bb99936843e51c06609e32e11
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-06-24 21:41:45 +0300

    Add automapper and task dto

---
+++ TaskManager/App_Start/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TaskManager.Models;
+using TaskManager.Models.Dto;
+
+namespace TaskManager.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<ProjectTask, ProjectTaskDto>();
+            Mapper.CreateMap<ProjectTaskDto, ProjectTask>();
+        }
+    }
+}
---
+++ TaskManager/Models/Dto/ProjectTaskDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Models.Dto
+{
+    public class ProjectTaskDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Deadline { get; set; }
+
+        [Required]
+        public Status Status { get; set; }
+
+        [Required]
+        public Priority Priority { get; set; }
+    }
+}
