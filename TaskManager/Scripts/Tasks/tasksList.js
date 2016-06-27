e5ef38a787816cb9a9f953c905c3ae3fbfcd0858
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-06-27 18:58:35 +0300

    Add delete action for tasks list

--- TaskManager/Scripts/Tasks/tasksList.js
+++ TaskManager/Scripts/Tasks/tasksList.js
@@ -10,7 +10,29 @@ $(document).ready(function () {
             { data: "name" },
             { data: "status.name" },
             { data: "priority.name" },
-            { data: "deadline" }
+            {
+                data: "deadline",
+                render: function (data, type, task) {
+                    return "<img align='right' class='js-delete' data-task-id=" + task.id + " src='../../Content/delete.png' alt='Delete' width='20'/>";
+                }
+            }
         ]
+    });
+
+    $("#tasks").on("click", ".js-delete", function () {
+        var button = $(this);
+
+        bootbox.confirm("Are you sure you want to delete this task?", function (result) {
+            if (result) {
+                $.ajax({
+                    url: "/api/tasks/" + button.attr("data-task-id"),
+                    method: "DELETE",
+                    success: function () {
+                        toastr.success("Task successfully deleted.");
+                        table.row(button.parents("tr")).remove().draw();
+                    }
+                });
+            }
+        });
     });
 });
